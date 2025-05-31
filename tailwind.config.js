cb9c9f53d5fd62fd06e1f641f06d0d3c24927a32
Bryan Fawcett <bryan@nyuchi.com>
2025-06-01 03:09:17 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./dist/client/**/*.{js,jsx}",
+    "./main.jsx",
+    "./App.jsx",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
